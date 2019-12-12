f88980ed5112bfe4470fe6bb730544f799b28103
Christian Fredericksen <christian.fredericksen@yahoo.com>
2019-12-12 23:53:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 #write your code here
-
+countdown_output = 10
 def countdown
+  until countdown_output == 0
+    puts "#{countdown_output} SECOND(S)!"
+    countdown_output -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
