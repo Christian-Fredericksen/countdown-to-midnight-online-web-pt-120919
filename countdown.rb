db9e5ab3459f552c24628f8ec0e7ce1cfa23d118
Christian Fredericksen <christian.fredericksen@yahoo.com>
2019-12-12 23:55:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 countdown_output = 10
 def countdown
-  until countdown_output == 0
+  while countdown_output > 0
     puts "#{countdown_output} SECOND(S)!"
     countdown_output -= 1
   end
