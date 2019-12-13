b3d8796e37229d4ba82f27ec9941034983023a1a
Christian Fredericksen <christian.fredericksen@yahoo.com>
2019-12-13 00:48:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,10 +2,8 @@
 #countdown_output = 10
 def countdown(countdown_output)
   while countdown_output > 0
-  
     puts "#{countdown_output} SECOND(S)!"
     countdown_output -= 1
-    Benchmark.measure (1)
   end
     
    "HAPPY NEW YEAR!"
